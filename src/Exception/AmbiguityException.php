e6098c53b07e91bfc87751223849661f6d99c8d9
Karim Cassam Chenaï <kcassam@gmail.com>
2024-02-27 23:05:58 +0100

    Amount and Rate

---
+++ src/Exception/AmbiguityException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace namespace OverSkillTeam\FinancialTools\Exception;
+
+use LogicException;
+
+class AmbiguityException extends LogicException
+{
+}
---
+++ src/Rate.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OverSkillTeam\FinancialTools;
+
+use JsonSerializable;
+use Stringable;
+
+readonly class Rate implements JsonSerializable, Stringable
+{
+    public function __construct(private float $rate)
+    {
+    }
+
+    public function toPercentage(): float
+    {
+        return $this->rate * 100;
+    }
+
+    public function toRate(): float
+    {
+        return $this->rate;
+    }
+
+    public static function fromRate(float $rate): self
+    {
+        return new self($rate);
+    }
+
+    public static function fromPercentage(float $percentage): self
+    {
+        return new self($percentage / 100);
+    }
+
+    public static function fromNullableRate(?float $rate, float $default = null): ?self
+    {
+        if (is_null($rate) && is_null($default)) {
+            return null;
+        }
+
+        if (is_null($rate)) {
+            return new self($default);
+        }
+
+        return new self($rate);
+    }
+
+    public static function fromNullablePercentage(?float $percentage, float $default = null): ?self
+    {
+        if (is_null($percentage) && is_null($default)) {
+            return null;
+        }
+
+        if (is_null($percentage)) {
+            return new self($default / 100);
+        }
+
+        return new self($percentage / 100);
+    }
+
+    public function __toString(): string
+    {
+        return (string) format_number_by_type($this->rate, 'rate_to_percentage', 2, true);
+    }
+
+    public function jsonSerialize(): float
+    {
+        return $this->rate;
+    }
+}
